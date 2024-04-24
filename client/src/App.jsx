ccaf0771fc7751d41c64124f65c5a3699473ecb4
rupal singhal <rupalsinghal03@gmail.com>
2024-04-24 20:22:27 +0530

    latest

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <div>App</div>
   )
 }
-
-export default App
