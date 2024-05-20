46377feaa168f547bfbc9c96a6f1dcf79bc4cfc1
rupal singhal <rupalsinghal03@gmail.com>
2024-05-20 22:07:18 +0530

    add functionality to the sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios'
 export default function SignUp() {
   const logoStyle = {
@@ -10,37 +11,42 @@ export default function SignUp() {
     borderRadius: '10px',
     cursor: 'pointer',
   };
-  const buttonStyle = {
-    background: 'linear-gradient(to right, #630a63, #6565ed)',
-    border: 'none',
-    color: 'white',
-    padding: '10px 20px',
-    borderRadius: '5px',
-    cursor: 'pointer',
-  };
+
+  const navigate = useNavigate()
   const [formData, setFormData] = useState({})
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [loading, setLoading] = useState(false)
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: e.target.value })
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() })
   }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      setErrorMessage('Please fill all the details!')
+    }
     try {
-      console.log('hello');
+      setLoading(true)
       const res = await axios.post('/api/auth/signup', formData, {
         headers: {
           'Content-Type': 'application/json'
         }
       });
-      const data = res.data;
-      console.log(data);
+      const data = await res.data;
+      if (data.success === false) {
+        return setErrorMessage(data.message)
+      }
+      setLoading(false)
+      navigate('/sign-in')
     } catch (error) {
+      setErrorMessage(error.message)
       // Handle error
       console.error(error);
+      setLoading(false)
     }
   }
-  
+
 
   return (
     <div className='container'>
@@ -66,11 +72,22 @@ export default function SignUp() {
               <label className="form-label">Your Password</label>
               <input type="password" className="form-control" placeholder='Password' id='password' onChange={handleChange} />
             </div>
-            <button type="submit" className="btn btn-primary w-100 mb-2" style={logoStyle} onClick={handleSubmit}>Sign Up</button>
+            <button type="submit" className="btn btn-primary w-100 mb-2" style={logoStyle} onClick={handleSubmit} disabled={loading}>
+              {loading ? (
+                <>
+                  <div>Loading...</div>
+                </>
+              ) : 'Sign Up'}
+            </button>
           </form>
           <div className="signin mt-2">
             <span>Have an account? <Link to="#">Sign in</Link></span>
           </div>
+          {errorMessage && (
+            <div className="error_field bg-red p-2 mt-4 text-danger border border-danger rounded ">
+              <p className='fw-bold'>{errorMessage}</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
